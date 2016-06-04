9c575dda753c1305abdad0dad56d08085e5a91bc
gjieoo <15202979397@163.com>
2016-06-04 17:42:51 +0800

    first commit program7

---
+++ src/collect_all_even.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function collect_all_even(collection) {
+
+    var evenArray=[];
+
+    for(var i=0;i<collection.length;i++){
+
+        if(collection[i]%2 === 0){
+
+            evenArray.push(collection[i]);
+        }
+    }
+
+    return evenArray;
+}
+
+module.exports = collect_all_even;
